9c921b9f47879890dee1acb3b74ddbda078a34ea
jaruizes <jalb80@gmail.com>
2021-03-18 16:31:22 +0100

    MVP

---
+++ bin/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const gitlabCloner = require('../lib/gitlab-cloner/gitlab-group-cloner');
+
+gitlabCloner.clone();
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    verbose: true,
+    testEnvironment: "node",
+    testMatch: [
+        "**/?(*.)+(spec|test).[jt]s?(x)"
+    ]
+};
